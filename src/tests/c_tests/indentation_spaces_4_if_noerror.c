c2d3cf84a2514ccf3c9c0afeaf3e46a8125759ac
luchiel <luchiella@yandex.ru>
2012-10-31 00:37:46 +0100

    add indentation_if C test

---
+++ src/tests/c_tests/indentation_spaces_4_if_noerror.c
@@ -0,0 +1,13 @@
+int main()
+{
+    if(a)
+        if(b)
+            if(c)
+                hello();
+            else if(d)
+                    hello();
+                else
+                    hello();
+        else
+            hello();
+}
