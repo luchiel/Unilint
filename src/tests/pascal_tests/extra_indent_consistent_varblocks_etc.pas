71580459b8dbdb950afe9d7622d86db88eabe84a
luchiel <luchiella@yandex.ru>
2012-11-02 06:49:58 +0100

    new extra_indent test (pascal)

---
+++ src/tests/pascal_tests/extra_indent_consistent_varblocks_etc.pas
@@ -0,0 +1,19 @@
+type
+  a = int;
+  
+procedure abaca;
+  const
+    MILLE: int = 1000;
+  var
+    i: integer;
+  begin
+    do_nothing();
+  end;
+  
+var
+  freedom: char;
+begin
+  for freedom := 0 to 10 do begin
+      //error next line :)
+  end;
+end.
